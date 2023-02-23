605e955a71fa8ffcd44192e53fafc9814fb1b632
HANH\Hanh <truehome919@gmail.com>
2023-02-23 16:12:30 +0700

    update

--- H_run_file.py
+++ H_run_file.py
@@ -15,16 +15,15 @@ from openpyxl import Workbook
 import os
 import select
 import H_function, H_functions, HR_timecard, HR_timecard_OT
-from H_functions import Logging#, execution_log, fail_log, error_log
+from H_functions import Logging, execution_log#, fail_log, error_log
 from simple_colors import *
 
-
-def MyExecution(domain_name):
+def MyExecution(domain,id,pw):
     error_menu = []
     error_screenshot = []
     
     try:
-        H_functions.access_hr(domain_name)
+        H_functions.access_hr(domain,id,pw)
     except:
         Logging("Cannot continue execution")
         error_menu.append("H_functions.access_hr")
@@ -49,24 +48,32 @@ def MyExecution(domain_name):
     
     
     hanh_log = {
-        # "execution_log": execution_log,
+        "execution_log": execution_log,
         # "fail_log": fail_log,
         # "error_log": error_log,
-        # "error_menu": error_menu
+        "error_menu": error_menu
     }
 
     return hanh_log
 
 
-def My_Execution(domain_name):
-    H_functions.access_hr(domain_name)
+def My_Execution(domain,id,pw):
+    H_functions.access_hr(domain,id,pw)
     HR_timecard.admin_and_user()
     #HR_timecard.timecard()
     #HR_timecard.time_card()
     #HR_timecard_OT.timecard_OT()
     #HR_timecard_OT.timecard_report()
 
-My_Execution("http:/groupware57.hanbiro.net/nhr/login")
+#My_Execution("http:/groupware57.hanbiro.net/nhr/login")
+#My_Execution("http:/qavn.hanbiro.net/nhr/login")
+My_Execution("http:/tg01.hanbiro.net/nhr/login","automationtest1","automationtest1!")
+#My_Execution("http:/tg02.hanbiro.net/nhr/login")
+#My_Execution("http:/groupware65.hanbiro.net/nhr/login")
+#My_Execution("http:/groupware66.hanbiro.net/nhr/login")
+#My_Execution("http:/imgrocky.hanbiro.net/nhr/login")
+
 #My_Execution("http:/gw.hanbirolinux.tk/nhr/login")
 #My_Execution("http:/global3.hanbiro.com/nhr/login")
+
 
